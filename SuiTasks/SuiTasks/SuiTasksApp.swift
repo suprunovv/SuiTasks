f246a7ee308d53717b275987e63cf8780170abfd
Suprunov Anton <suprunovvanton666@gmail.com>
2024-05-01 11:49:35 +0300

    no message

---
+++ SuiTasks/SuiTasks/SuiTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuiTasksApp.swift
+//  SuiTasks
+//
+//  Created by MacBookPro on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuiTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
